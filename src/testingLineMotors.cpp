2388e79e3867c5d2d0a8f87f211b5e6ec05a6796
Martinlinux1 <matko.micuch@gmail.com>
2018-10-04 16:07:40 +0200

    Add files via upload

---
+++ src/testingLineMotors.cpp
@@ -0,0 +1,46 @@
+#include "include.h"
+#include "Motors.h"
+#include "LightSensors.h"
+
+int motorPins[4] = {2, 3, 4, 5};
+int lightSensorsPins[8] = {13, 12, 11, 10, 9, 8, 7, 6};
+
+Motors motors(motorPins, 255);
+LightSensors lightSensors(lightSensorsPins);
+
+bool isLine(short *sensor);
+
+int baseSpeed = 150;
+
+void setup() {
+
+}
+
+void loop() {
+  short lightSensor;
+  if (isLine(&lightSensor)) {
+    if (lightSensor == 0 || lightSensor == 1) {
+      motors.moveTank(baseSpeed, -baseSpeed);
+    }
+
+    else if (lightSensor == 2 || lightSensor == 3) {
+      motors.moveTank(-baseSpeed, baseSpeed);
+    }
+  }
+
+  else {
+    motors.forward(baseSpeed, baseSpeed);
+  }
+}
+
+bool isLine(short *sensor) {
+  for (unsigned int i = 0; i < arrLen(lightSensorsPins); i++) {
+    if (lightSensors.getLight(i) == 0) {
+      *sensor = i;
+      return true;
+    }
+  }
+
+  *sensor = -1;
+  return false;
+}
