c2856ed1b6d1b5f3cc910194724bf1031197eaaa
Martinlinux1 <matko.micuch@gmail.com>
2018-10-06 11:48:08 +0200

    Ultrasonic sensors working.

--- src/testingUltrasonic.cpp
+++ src/testingUltrasonic.cpp
@@ -1,8 +1,8 @@
 #include "include.h"
 #include "UltrasonicSensors.h"
 
-int echoPins[5] = {47, 48, 31, 32, 51};
-int trigPins[5] = {29, 30, 49, 50, 33};
+int echoPins[5] = {47, 30, 31, 32, 51};
+int trigPins[5] = {29, 46, 49, 50, 33};
 
 UltrasonicSensors prox(trigPins, echoPins);
 
@@ -10,16 +10,18 @@ float distanceCm(int sensor);
 
 
 void setup() {
+  for (unsigned int i = 0; i < arrLen(trigPins); i++) {
+    pinMode(trigPins[i], OUTPUT);
+  }
+
+  for (unsigned int i = 0; i < arrLen(echoPins); i++) {
+    pinMode(echoPins[i], INPUT);
+  }
   Serial.begin(9600);
 }
 
 
 void loop() {
-  Serial.print("Ultrasonic sensors raw: ");
-  for (int i = 0; i < 5; i++) {
-    Serial.println(distanceCm(i));
-  }
-
   Serial.print("Ultrasonic sensors obj: ");
   for (int i = 0; i < 5; i++) {
     Serial.println(prox.distanceCm(i));
